e13902af7adcef82e646dc3eb16bb521a457f826
SucreauxVanille <study.dreammate@gmail.com>
2026-06-17 02:54:20 +0900

    Update assets.js

--- assets.js
+++ assets.js
@@ -9,5 +9,30 @@ const assets = {
         image: "images/objects/bed.png",
         animated: false
     },
+himiko: {
 
+    up: [
+        "images/characters/himiko_back1.png",
+        "images/characters/himiko_back2.png"
+    ],
+
+    down: [
+        "images/characters/himiko_front1.png",
+        "images/characters/himiko_front2.png"
+    ],
+
+    left: [
+        "images/characters/himiko_left1.png",
+        "images/characters/himiko_left2.png"
+    ],
+
+    right: [
+        "images/characters/himiko_right1.png",
+        "images/characters/himiko_right2.png"
+    ],
+
+    animated: true
+
+},
+    
 }
