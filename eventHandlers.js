5bb202ff32c15b1a57e2fd7ef01e247236f9a4f1
SucreauxVanille <study.dreammate@gmail.com>
2026-06-17 18:18:27 +0900

    Update eventHandlers.js

--- eventHandlers.js
+++ eventHandlers.js
@@ -1,3 +1,15 @@
+function setFlag(flagName){
+
+    gameState.flags[flagName] = true;
+
+}
+
+function hasFlag(flagName){
+
+    return gameState.flags[flagName] === true;
+
+}
+
 const eventHandlers = {
 
     mirror: mirrorEvent
