878ac5e2b939e1e52df4020c7731be76aa03fb56
SucreauxVanille <study.dreammate@gmail.com>
2026-06-17 22:50:37 +0900

    Update assets.js

--- assets.js
+++ assets.js
@@ -34,6 +34,10 @@ hero: {
         image: "images/objects/bed.png",
         animated: false
     },
+    stamp: {
+        image: "images/objects/stamp.png",
+        animated: false
+    },
     himiko: {
 
         animated: true,
@@ -56,6 +60,57 @@ hero: {
         right: [
             "images/characters/himiko_right1.png",
             "images/characters/himiko_right2.png"
+        ]
+
+    },
+    iyo: {
+
+        animated: true,
+
+        down: [
+            "images/characters/iyo_front1.png",
+            "images/characters/iyo_front2.png"
+        ],
+
+        up: [
+            "images/characters/iyo_back1.png",
+            "images/characters/iyo_back2.png"
+        ],
+
+        left: [
+            "images/characters/iyo_front1.png",
+            "images/characters/iyo_front2.png"
+        ],
+
+        right: [
+            "images/characters/iyo_front1.png",
+            "images/characters/iyo_front2.png"
+        ]
+
+    },
+   
+        brother: {
+
+        animated: true,
+
+        down: [
+            "images/characters/brother_front1.png",
+            "images/characters/brother_front2.png"
+        ],
+
+        up: [
+            "images/characters/brother_back1.png",
+            "images/characters/brother_back2.png"
+        ],
+
+        left: [
+            "images/characters/brother_left1.png",
+            "images/characters/brother_left2.png"
+        ],
+
+        right: [
+            "images/characters/brother_right1.png",
+            "images/characters/brother_right2.png"
         ]
 
     }
