dfbe43138706122a035f8abda7297a2e9681736b
SucreauxVanille <study.dreammate@gmail.com>
2026-06-17 17:09:20 +0900

    Create input.js

---
+++ input.js
@@ -0,0 +1,153 @@
+//キー操作
+document.addEventListener("keydown", (e)=>{
+if(isMessageOpen){
+    return;
+}
+    switch(e.key){
+
+        case "ArrowUp":
+
+            if(!pressedKeys.up){
+                movePlayer(0,-1,"up");
+            }
+
+            pressedKeys.up = true;
+            break;
+
+        case "ArrowDown":
+
+            if(!pressedKeys.down){
+                movePlayer(0,1,"down");
+            }
+
+            pressedKeys.down = true;
+            break;
+
+        case "ArrowLeft":
+
+            if(!pressedKeys.left){
+                movePlayer(-1,0,"left");
+            }
+
+            pressedKeys.left = true;
+            break;
+
+        case "ArrowRight":
+
+            if(!pressedKeys.right){
+                movePlayer(1,0,"right");
+            }
+
+            pressedKeys.right = true;
+            break;
+    }
+
+});
+
+document.addEventListener("keyup", (e)=>{
+
+    switch(e.key){
+
+        case "ArrowUp":
+            pressedKeys.up = false;
+            break;
+
+        case "ArrowDown":
+            pressedKeys.down = false;
+            break;
+
+        case "ArrowLeft":
+            pressedKeys.left = false;
+            break;
+
+        case "ArrowRight":
+            pressedKeys.right = false;
+            break;
+    }
+
+});
+
+//ボタン
+function pressDirection(direction){
+if(isMessageOpen){
+    return;
+}
+    if(!pressedKeys[direction]){
+
+        switch(direction){
+
+            case "up":
+                movePlayer(0,-1,"up");
+                break;
+
+            case "down":
+                movePlayer(0,1,"down");
+                break;
+
+            case "left":
+                movePlayer(-1,0,"left");
+                break;
+
+            case "right":
+                movePlayer(1,0,"right");
+                break;
+        }
+    }
+
+    pressedKeys[direction] = true;
+}
+
+function releaseDirection(direction){
+    pressedKeys[direction] = false;
+}
+
+const buttons = [
+    ["upBtn","up"],
+    ["downBtn","down"],
+    ["leftBtn","left"],
+    ["rightBtn","right"]
+];
+buttons.forEach(([id, direction])=>{
+
+    const btn = document.getElementById(id);
+
+    btn.addEventListener("pointerdown", ()=>{
+
+        pressDirection(direction);
+
+    });
+
+    btn.addEventListener("pointerup", ()=>{
+
+        releaseDirection(direction);
+
+    });
+
+    btn.addEventListener("pointerleave", ()=>{
+
+        releaseDirection(direction);
+
+    });
+
+});
+
+//インターバル
+setInterval(()=>{
+
+    if(pressedKeys.up){
+        movePlayer(0,-1,"up");
+    }
+
+    else if(pressedKeys.down){
+        movePlayer(0,1,"down");
+    }
+
+    else if(pressedKeys.left){
+        movePlayer(-1,0,"left");
+    }
+
+    else if(pressedKeys.right){
+        movePlayer(1,0,"right");
+    }
+
+}, 150);
