4bcad43cd08baf11fd721eb0cf84fe4c650e3f43
SucreauxVanille <study.dreammate@gmail.com>
2026-06-18 22:33:26 +0900

    Update effect.js

--- effect.js
+++ effect.js
@@ -14,3 +14,25 @@ function fadeIn(){
     screenEffect.style.opacity = 0;
 
 }
+
+function flashScreen(){
+
+    screenEffect.style.backgroundColor =
+        "white";
+
+    screenEffect.style.opacity = 1;
+
+    setTimeout(()=>{
+
+        screenEffect.style.opacity = 0;
+
+    },150);
+
+    setTimeout(()=>{
+
+        screenEffect.style.backgroundColor =
+            "black";
+
+    },300);
+
+}
