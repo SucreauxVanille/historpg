71609375b4917c17b4cc8c991c7611cadc60559b
SucreauxVanille <study.dreammate@gmail.com>
2026-06-18 22:30:20 +0900

    Update game.js

--- game.js
+++ game.js
@@ -475,6 +475,6 @@ document
     document.getElementById("titleScreen").style.display = "none";
     document.getElementById("gameScreen").style.display = "block";
 
-    render();
-    startMessage(events.prologueStart);
+render();
+startOpening();
 });
