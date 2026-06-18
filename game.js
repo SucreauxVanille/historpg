9218f352cfd044d9425ab53cb52a457288a136ab
SucreauxVanille <study.dreammate@gmail.com>
2026-06-18 23:17:10 +0900

    Update game.js

--- game.js
+++ game.js
@@ -196,64 +196,6 @@ function render(){
 }
 
 
-//メッセージウインドウ
-let messages = [];
-let messageIndex = 0;
-let messageFinishedCallback = null;
-function showMessage(text){
-
-    messageText.textContent = text;
-    messageBox.style.display = "block";
-
-    isMessageOpen = true;
-    canAdvanceMessage = false;
-
-    setTimeout(()=>{
-        canAdvanceMessage = true;
-    }, 500);
-}
-
-function startMessage(messageArray, onFinish = null){
-    messages = messageArray;
-    messageIndex = 0;
-
-    messageFinishedCallback = onFinish;
-    showMessage(messages[0]);
-}
-
-function hideMessage(){
-    document.getElementById("messageBox").style.display = "none";
-    isMessageOpen = false;
-}
-function nextMessage(){
-    messageIndex++;
-if(messageIndex >= messages.length){
-
-    hideMessage();
-const callback = messageFinishedCallback;
-messageFinishedCallback = null;
-
-if(callback){
-    callback();
-}
-    return;
-}
-    showMessage(messages[messageIndex]);
-}
-messageBox.addEventListener("pointerdown", ()=>{
-
-    if(!isMessageOpen){
-        return;
-    }
-
-    if(!canAdvanceMessage){
-        return;
-    }
-
-    nextMessage();
-
-});
-
 //地雷式イベント
 function getTileEvent(x, y){
 
