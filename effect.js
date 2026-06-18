b7198c707a9de36fb363518fd5310aa9c03a0847
SucreauxVanille <study.dreammate@gmail.com>
2026-06-18 22:08:04 +0900

    Create effect.js

---
+++ effect.js
@@ -0,0 +1,16 @@
+const screenEffect =
+    document.getElementById(
+        "screenEffect"
+    );
+
+function fadeOut(){
+
+    screenEffect.style.opacity = 1;
+
+}
+
+function fadeIn(){
+
+    screenEffect.style.opacity = 0;
+
+}
