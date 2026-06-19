970e274cb593da4c89eafccde7cefe8f8630b900
SucreauxVanille <study.dreammate@gmail.com>
2026-06-20 01:16:48 +0900

    Create enemyData.js

---
+++ battle/enemyData.js
@@ -0,0 +1,24 @@
+const enemies = {
+
+    boar: {
+        name: "狂暴イノシシ",
+        hp: 20,
+        atk: 5,
+        exp: 20
+    },
+
+    deer: {
+        name: "狂暴オオツノジカ",
+        hp: 35,
+        atk: 8,
+        exp: 40
+    },
+
+    naumann: {
+        name: "狂暴ナウマンゾウ",
+        hp: 80,
+        atk: 15,
+        exp: 100
+    }
+
+};
