3f6bf282c6cd4c13e5468f06162e4ce91d1d1db6
SucreauxVanille <study.dreammate@gmail.com>
2026-06-19 10:57:42 +0900

    Create nojiriEvents.js

---
+++ nojiriEvents.js
@@ -0,0 +1,21 @@
+function returnMirrorEvent(){
+
+    showMessage(
+        "卑弥呼ハウスへ戻る機能は未実装です。"
+    );
+
+}
+function nojiriTutorialEvent(){
+
+    showMessage(
+        "卑弥呼「ここが野尻湖じゃ」"
+    );
+
+}
+function naumannBossEvent(){
+
+    showMessage(
+        "巨大なナウマンゾウが立ちはだかっている。"
+    );
+
+}
