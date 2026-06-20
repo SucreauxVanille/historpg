d2255d1e18e3f6d4a54768b0929194eb737032ee
SucreauxVanille <study.dreammate@gmail.com>
2026-06-20 22:19:32 +0900

    Update nojiriEvents.js

--- nojiriEvents.js
+++ nojiriEvents.js
@@ -12,14 +12,13 @@ function nojiriTutorialEvent(){
 
     startMessage(
         [
-            "卑弥呼「む！？気をつけよ！」",
-            "卑弥呼「狂暴化した獣のようじゃ！」",
+            "卑弥呼「む！？この時代にも狂暴化した獣がいるようじゃ！」",
             "卑弥呼「やむを得ぬ！戦うぞ！」"
         ],
         () => {
 
             startBattle("boar");
-
+            endEvent
         }
     );
 
