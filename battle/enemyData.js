c477dad889b10bb9dffc15f59ad45e5da7813e13
SucreauxVanille <study.dreammate@gmail.com>
2026-06-21 20:21:15 +0900

    Update enemyData.js

--- battle/enemyData.js
+++ battle/enemyData.js
@@ -5,21 +5,24 @@ const enemies = {
         hp: 20,
         atk: 5,
         exp: 20,
-        image: "battle/images/boar.png"
+        image: "battle/images/boar.png",
+        boss: false
     },
 
     deer: {
         name: "狂暴オオツノジカ",
         hp: 35,
         atk: 8,
-        exp: 40
+        exp: 40,
+        boss: false
     },
 
     naumann: {
         name: "狂暴ナウマンゾウ",
         hp: 80,
         atk: 15,
-        exp: 100
+        exp: 100,
+        boss: true
     }
 
 };
