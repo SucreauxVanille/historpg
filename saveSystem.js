729cc5f62b4024caa821a5e3d033d5d0979952cf
SucreauxVanille <study.dreammate@gmail.com>
2026-06-24 02:51:41 +0900

    Update saveSystem.js

--- saveSystem.js
+++ saveSystem.js
@@ -6,13 +6,30 @@ const PROGRESS = {
     OBSIDIAN_OBTAINED: 4,
     DEMO_CLEAR: 5
 };
-const PROGRESS_WORDS = {
+const SAVE_WORDS = {
     0: "はる",
     1: "すな",
     2: "かに",
     3: "そら",
     4: "とり",
-    5: "ほし"
+    5: "ほし",
+    6: "くも",
+    7: "さる",
+    8: "たに",
+    9: "あき"
+};
+
+const WORD_TO_NUMBER = {
+    "はる": "0",
+    "すな": "1",
+    "かに": "2",
+    "そら": "3",
+    "とり": "4",
+    "ほし": "5",
+    "くも": "6",
+    "さる": "7",
+    "たに": "8",
+    "あき": "9"
 };
 function applyProgress(progress){
 
@@ -51,6 +68,7 @@ function createSaveCode(){
     );
 
 }
+
 function parseSaveCode(code){
 
     const parts = code.split("-");
@@ -58,6 +76,67 @@ function parseSaveCode(code){
     return {
         exp: Number(parts[0]),
         progress: Number(parts[1])
+    };
+
+}
+
+//暗号化
+function encodeSaveCode(){
+
+    const expCode =
+        String(playerStatus.exp)
+        .split("")
+        .map(num => SAVE_WORDS[num])
+        .join("");
+
+    const progressCode =
+        SAVE_WORDS[gameState.progress];
+
+    return (
+        expCode +
+        "いのり" +
+        progressCode
+    );
+
+}
+
+function decodeSaveCode(spell){
+
+    const parts =
+        spell.split("いのり");
+
+    const expSpell =
+        parts[0];
+
+    const progressSpell =
+        parts[1];
+
+    let exp = "";
+
+    for(
+        let i = 0;
+        i < expSpell.length;
+        i += 2
+    ){
+
+        const word =
+            expSpell.slice(i, i + 2);
+
+        exp +=
+            WORD_TO_NUMBER[word];
+
+    }
+
+    const progress =
+        Number(
+            WORD_TO_NUMBER[
+                progressSpell
+            ]
+        );
+
+    return {
+        exp: Number(exp),
+        progress
     };
 
 }
