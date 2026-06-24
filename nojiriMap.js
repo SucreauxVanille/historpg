ac7f81d073be9e6d4b7f73ec8bef5859b1904e76
SucreauxVanille <study.dreammate@gmail.com>
2026-06-25 01:32:34 +0900

    Update nojiriMap.js

--- nojiriMap.js
+++ nojiriMap.js
@@ -91,7 +91,7 @@ maps.nojiriLake = {
             id: "naumann",
             x: 2,
             y: 0,
-            active: !hasFlag("naumannDefeated"),
+            active: true,
             solid: true,
             direction: "down",
             event: "naumannBoss"
@@ -101,7 +101,7 @@ maps.nojiriLake = {
             id: "obsidian",
             x: 1,
             y: 0,
-            active: !hasFlag("obsidianObtained"),
+            active: true,
             solid: true,
             event: "obsidian"
         }
