3ecfd7f9e448ac54f7199fab5466f18d5fab0dea
SucreauxVanille <study.dreammate@gmail.com>
2026-06-28 16:08:22 +0900

    Update battleManager.js

--- battle/battleManager.js
+++ battle/battleManager.js
@@ -11,3 +11,18 @@ let battleState = "player"; // player / enemy
 let currentEnemy = null;
 
 let battleFinishedCallback = null;
+
+//UI管理
+function updateBattleUI(){
+    const isCommand = battlePhase === "command";
+
+    document.getElementById("battleCommand")
+        .style.display = isCommand ? "flex" : "none";
+    document.getElementById("battleLog")
+        .style.display = isCommand ? "none" : "block";
+}
+
+function setBattlePhase(phase){
+    battlePhase = phase;
+    updateBattleUI();
+}
