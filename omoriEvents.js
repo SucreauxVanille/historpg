412b37997cc2cdf06629f1010d83a6982985fb95
SucreauxVanille <study.dreammate@gmail.com>
2026-07-01 21:20:03 +0900

    Update omoriEvents.js

--- omoriEvents.js
+++ omoriEvents.js
@@ -1,3 +1,37 @@
+function omoriShellEvent(){
+
+    if(hasFlag("omoriShellFinished")){
+        endEvent();
+        return;
+    }
+
+    startMessage(
+        [
+            "卑弥呼「ふむ…海がすぐそばなのじゃな」",
+            "卑弥呼「確かにこれなら貝とも関係がありそうじゃ」",
+            "？？？「カタカタ…」",
+            "卑弥呼「これ、おかしな声を出すでない」",
+            "？？？「カタカタカタ…！」",
+            "卑弥呼「なんじゃなんじゃ！？」"
+        ],
+        () => {
+
+            startBattle(
+                ["shell"],
+                (result) => {
+
+                    if(result === "win"){
+                        setFlag("omoriShellFinished");
+                        gameState.progress = PROGRESS.OMORI_SHELL;
+                    }
+                }
+            );
+
+            endEvent();
+        }
+    );
+}
+
 function meetMorseEvent(){
     startMessage(
         [
