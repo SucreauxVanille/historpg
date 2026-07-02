57911bff0505acb6bcbfc7a66b57e751e2f290ab
SucreauxVanille <study.dreammate@gmail.com>
2026-07-02 16:38:22 +0900

    Update skills.js

--- battle/skills.js
+++ battle/skills.js
@@ -2,6 +2,7 @@ const skills = {
 
     sandAttack: {
         name: "砂けむり",
+        id: "sandAttack",
         learnLevel: 5,
         mp: 3,
         type: "debuff"
@@ -9,6 +10,7 @@ const skills = {
 
     powerCharge: {
         name: "力ため",
+        id: "powerCharge",
         learnLevel: 10,
         mp: 4,
         type: "buff"
