2e5e0e06424537144bcfe986bea27089a2dc72ad
SucreauxVanille <study.dreammate@gmail.com>
2026-07-02 21:44:00 +0900

    Update skills.js

--- battle/skills.js
+++ battle/skills.js
@@ -1,27 +1,40 @@
 const skills = {
 
-    sandAttack: {
-        name: "砂けむり",
-        id: "sandAttack",
-        learnLevel: 5,
-        mp: 3,
-        type: "debuff",
-        castMessage:
-        "ゆうしゃは 砂けむり をつかった！",
-        successMessage:
-        "敵の攻撃が よわまった！"
+    sandAttack:{
+
+        name:"砂けむり",
+        learnLevel:5,
+
+        mp:3,
+
+        castMessage:"ゆうしゃは 砂けむりをおこした！",
+        successMessage:"敵の攻撃が よわまった！",
+
+        target:"enemy",
+        stat:"atk",
+        method:"multiply",
+        value:0.8,
+        duration:"battle"
+
     },
 
-    powerCharge: {
-        name: "力ため",
-        id: "powerCharge",
-        learnLevel: 10,
-        mp: 4,
-        type: "buff",
-        castMessage:
-        "ゆうしゃは 力をためた！",
-        successMessage:
-        "次の攻撃に　力をこめる！"
+    powerCharge:{
+
+        name:"力ため",
+        learnLevel:10,
+
+        mp:4,
+
+        castMessage:"ゆうしゃは 力をためた！",
+
+        successMessage:"次の攻撃に 力をこめている！",
+
+        target:"player",
+        stat:"atk",
+        method:"multiply",
+        value:2.2,
+        duration:"nextAttack"
+
     }
 
 };
