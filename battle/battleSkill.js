e677e23266b52de05de287f6d66b5c4088342a7d
SucreauxVanille <study.dreammate@gmail.com>
2026-07-02 22:35:44 +0900

    Update battleSkill.js

--- battle/battleSkill.js
+++ battle/battleSkill.js
@@ -9,3 +9,34 @@ function getAvailableSkills(){
         );
 
 }
+function applySkill(skill){
+
+    let target;
+
+    switch(skill.target){
+
+        case "player":
+            target = playerStatus;
+            break;
+
+        case "partner":
+            target = himikoStatus;
+            break;
+
+        case "enemy":
+            target = currentEnemy;
+            break;
+    }
+
+    switch(skill.method){
+
+        case "multiply":
+            // 後で実装
+            break;
+
+        case "add":
+            // 後で実装
+            break;
+    }
+
+}
