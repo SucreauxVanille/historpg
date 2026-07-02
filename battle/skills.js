8c7f19153b4e4d165b8cebd2638328bdf76fa96f
SucreauxVanille <study.dreammate@gmail.com>
2026-07-02 14:44:41 +0900

    Create skills.js

---
+++ battle/skills.js
@@ -0,0 +1,17 @@
+const skills = {
+
+    sandAttack: {
+        name: "砂けむり",
+        learnLevel: 5,
+        mp: 3,
+        type: "debuff"
+    },
+
+    powerCharge: {
+        name: "力ため",
+        learnLevel: 10,
+        mp: 4,
+        type: "buff"
+    }
+
+};
