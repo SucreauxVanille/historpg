bde4307bfa3331c44de790a6f40c20f759780411
SucreauxVanille <study.dreammate@gmail.com>
2026-07-11 02:02:27 +0900

    Update houseState.js

--- houseState.js
+++ houseState.js
@@ -18,3 +18,34 @@ function updateIyo(){
     }
 
 }
+const HOUSE_STATE = {
+
+    OPENING: 0,
+
+    NOJIRI_PLAYING: 1,
+
+    OMORI_READY: 2,
+
+    OMORI_PLAYING: 3
+
+};
+
+function getHouseState(){
+
+    switch(gameState.progress){
+
+        case PROGRESS.IYO_APPEARED:
+        case PROGRESS.NOJIRI_TUTORIAL:
+        case PROGRESS.NAUMANN_DEFEATED:
+        case PROGRESS.OBSIDIAN_OBTAINED:
+            return HOUSE_STATE.NOJIRI_PLAYING;
+
+        case PROGRESS.DEMO_CLEAR:
+            return HOUSE_STATE.OMORI_READY;
+
+        default:
+            return HOUSE_STATE.OPENING;
+
+    }
+
+}
