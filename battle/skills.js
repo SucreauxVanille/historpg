e44860f8a0b261c013eb88490730ed55db64c19a
SucreauxVanille <study.dreammate@gmail.com>
2026-07-15 00:06:02 +0900

    Update skills.js

--- battle/skills.js
+++ battle/skills.js
@@ -51,6 +51,60 @@ const skills = {
         value:2.4,
         duration:"nextAttack"
 
+    },
+
+    mudShot:{
+
+        name:"泥とばし",
+        learnLevel:19,
+
+        mp:8,
+
+        castMessage:"ゆうしゃは 泥を投げつけた！",
+        successMessage:"敵の攻撃が かなり よわまった！",
+
+        target:"enemy",
+        stat:"atk",
+        method:"multiply",
+        value:0.65,
+        duration:"battle"
+
+    },
+    
+    healing:{
+
+    name:"集中回復",
+    learnLevel:25,
+
+    mp:7,
+
+    castMessage:"ゆうしゃは 回復に集中した！",
+    successMessage:"ゆうしゃのキズが回復した！",
+
+    target:"player",
+    stat:"hp",
+    method:"add",
+    value:80
+
+    },
+    soulCharge:{
+
+        name:"気合込め",
+        learnLevel:30,
+
+        mp:8,
+
+        castMessage:"ゆうしゃは 全力で 気合を込めた！",
+
+        successMessage:"次の攻撃に 魂をのせる！",
+
+        target:"player",
+        stat:"atk",
+        method:"multiply",
+        value:3,
+        duration:"nextAttack"
+
     }
+    
 
 };
