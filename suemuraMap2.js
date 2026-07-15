5db741a622a95bde19e110ba30563c045a47dbf1
SucreauxVanille <study.dreammate@gmail.com>
2026-07-16 00:28:52 +0900

    Create suemuraMap2.js

---
+++ suemuraMap2.js
@@ -0,0 +1,97 @@
+maps.suemuraMap2 = {
+    name: "古墳時代　陶邑",
+
+    description: [
+        "現在の大阪府。",
+        "焼き物を作るための",
+        "窯を多く設置した集落。"
+    ],
+    allowHimikoFollower: true,
+    encounterRate: 3,
+    clearEncounterRate: 1,
+    clearFlag: "haniwaDefeated",
+    maxEncounterCount: 3,
+    encounterTable: [
+        "boar",
+        "rabbit"
+    ],
+    
+    tiles: [
+
+        [7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7],
+        [5,5,4,2,3,5,5,5,5,7,7,7,7,5,5,5,5,5,5],
+        [7,2,2,2,2,2,2,2,2,2,5,5,5,2,2,2,2,3,7],
+        [7,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,3,7],
+        [7,2,2,2,2,7,7,2,2,2,2,2,2,2,2,2,2,2,7],
+
+        [7,2,2,2,2,2,7,2,2,2,2,2,2,2,2,2,2,4,7],
+        [7,2,2,2,7,2,2,7,7,2,2,2,2,2,2,2,2,2,7],
+        [7,2,2,2,7,2,2,2,2,2,2,2,7,7,7,2,2,2,7],
+        [7,2,2,2,7,2,2,2,2,2,2,2,7,2,2,2,2,2,7],
+        [2,2,2,2,7,2,2,2,2,2,2,7,2,2,2,2,2,2,2],
+        
+        [7,7,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,7],
+        [7,2,2,2,2,2,7,2,2,2,2,2,2,2,2,2,2,2,7],
+        [7,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,7],
+        [7,2,2,2,7,7,2,2,2,7,7,2,2,2,2,2,2,2,7],
+        [7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7]
+
+    ],
+    
+    objects: [
+        {
+            id: "mob",
+            x: 18,
+            y: 7,
+            active: true,
+            solid: true,
+            direction: "down",
+            event: "omorimob1"
+        },
+
+        {
+            id: "mobF",
+            x: 10,
+            y: 13,
+            active: true,
+            solid: true,
+            direction: "right",
+            event: "omorimobF1"
+        },
+
+        {
+            id: "houseL",
+            x: 8,
+            y: 11,
+            active: true,
+            solid: true,
+            event: "suemuraHouse"
+        },
+        {
+            id: "houseR",
+            x: 9,
+            y: 11,
+            active: true,
+            solid: true,
+            event: "suemuraHouse"
+        },
+      
+        {
+            id: "spring",
+            x: 15,
+            y: 10,
+            active: true,
+            solid: true,
+            event: "recoverySpring"
+        }
+    ],
+
+
+    tileEvents: [
+        {
+            x: 19,
+            y: 9,
+            event: "moveSuemura1"
+        }
+    ]
+};
