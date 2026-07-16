ce9dfc55625935b9a46f480271b2b94ab5209cce
SucreauxVanille <study.dreammate@gmail.com>
2026-07-17 01:32:17 +0900

    Update suemura2Events.js

--- suemura2Events.js
+++ suemura2Events.js
@@ -1,7 +1,7 @@
 async function yongEvent(){
     await startMessage([
-        "ヨン「こんにちはアル！」",
-        "ヨン「今は仮イベントアル！」"
+        "渡来人「こんにちはアル！」",
+        "渡来人「僕はヨン、海の向こうから来たアル」"
     ]);
     endEvent();
 }
